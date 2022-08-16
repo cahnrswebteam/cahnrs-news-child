c2c065f8e1a66dbdd6b23d2dc4cdd60518f1daab
Efren Vasquez <efren.vasquez@wsu.edu>
2022-08-16 13:42:25 -0500

    register admin stylesheet

--- functions.php
+++ functions.php
@@ -24,4 +24,17 @@ function my_theme_enqueue_styles() {
     );
 }
 
+/*=====================================
+
+
+Register Admin styles
+
+
+========================================*/
+function load_admin_styles() {
+    wp_enqueue_style( 'admin_css_foo', get_stylesheet_directory_uri() . '/admin/css/admin-styles.css', false );
+    wp_enqueue_style( 'admin_css_bar', get_stylesheet_directory_uri() . '/admin-style-bar.css', false, '1.0.0' );
+}
+
+add_action( 'admin_enqueue_scripts', 'load_admin_styles' );
 
