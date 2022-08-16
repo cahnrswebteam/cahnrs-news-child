16075afb23ee965d494fb667075e00ff5f22bec8
Efren Vasquez <efren.vasquez@wsu.edu>
2022-08-16 13:41:56 -0500

    register child theme styles

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php 
+
+/*=====================================
+
+
+Register Child Theme Style
+
+
+========================================*/
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function my_theme_enqueue_styles() {
+    $parenthandle = 'wsuwp-theme-wds'; 
+    $theme = wp_get_theme();
+
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(), 
+        $theme->parent()->get('Version')
+    );
+
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') 
+    );
+}
+
+
